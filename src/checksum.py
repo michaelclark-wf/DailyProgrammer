609ddef77a288b362cce5781713a008b12d5845b
Michael.Clark <Michael.Clark@webfilings.com>
2013-11-18 18:35:18 -0600

    Fletch16 Hash. Cleaned up algorithm. Use Python's Map funciton. Removed Class. Cleaned up input & Output.

--- src/checksum.py
+++ src/checksum.py
@@ -10,52 +10,19 @@
     # 1 D330
     # 2 D23E
     # 3 404D
+
 import sys
 
-class Fletcher16Input:
-    numLines = 0
-    lines    = []
-    out      = []
-
-
-
 def main():
-    data = get_input()
-    for i in xrange(0,len(data.lines)):
-        line = data.lines[i]
-        line_len = len(line)
-        ascii_line = line_to_ascii(line)
-        fletch = fletch16(ascii_line, line_len)
-        data.out.append("%d %s" % (i, fletch))
-    send_output(data.out)
-    return
+    i = 1
+    for s in sys.stdin.readlines()[1:]:
+        print i,fletcher_hash_16(s)
+        i+=1
 
-def get_input():
-    inputData = Fletcher16Input()
-    lines     = sys.stdin.readlines()
-    # inputData.numLines = int(lines[0])  #first line will be integer representing total # of checksums to preform
-    inputData.lines  = lines[1:] #fill data array with remaining lines
-    return inputData
-
-def send_output(output):
-    for line_out in output:
-        print line_out
-    return
-
-def line_to_ascii(line):
-    ret = []
-    for char in line:
-        ret.append(ord(char))
-    return ret
-
-def fletch16(data, count):
-    sum1, sum2 = 0,0
-    for i in xrange(0,count):
-        sum1 = (sum1 + data[i]) % 255
-        sum2 = (sum2 + sum1) % 255
-    ret = (sum2 << 8) | sum1
-    return hex(ret).split('x')[1]
-
+def fletcher_hash_16(s):
+    sum1 = sum(map(ord,s)) % 255
+    sum2 = (sum(map(ord,s)) + sum1) % 255
+    return hex((sum2 << 8) | sum1)
 
 if __name__ == '__main__':
     main()
