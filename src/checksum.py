e80ebc3c3849d7970ef7145ccfe196415f01144d
Michael.Clark <Michael.Clark@webfilings.com>
2013-11-24 19:50:56 -0600

    Checksum optimized. WIP, still not calculating 100%.'

--- src/checksum.py
+++ src/checksum.py
@@ -16,13 +16,41 @@ import sys
 def main():
     i = 1
     for s in sys.stdin.readlines()[1:]:
-        print i,fletcher_hash_16(s)
+        print i,fletcher_hash_16_optimized(s)
         i+=1
 
 def fletcher_hash_16(s):
     sum1 = sum(map(ord,s)) % 255
     sum2 = (sum(map(ord,s)) + sum1) % 255
     return hex((sum2 << 8) | sum1)
+
+def fletcher_hash_16_optimized(s):
+    words = len(s.split(' '))
+    ascii_sum = sum(map(ord, s))
+    print "words:",words
+    print "ascii_sum:",ascii_sum
+    # slen = slen > 359 ? 359 : slen  #why isn't ternary working!?
+    sum1,sum2 = 0,0
+    while words:
+        tlen = words
+        if words <= 359:
+            tlen=359
+        words = words - tlen
+        while tlen:
+            ascii_sum = ascii_sum + 1
+            sum1 = sum1 + ascii_sum
+            sum2 = sum2 + sum1
+            tlen = tlen - 1
+
+    sum1 = (sum1 & 0xffff) + (sum >> 16)
+    sum2 = (sum2 & 0xffff) + (sum >> 16)
+    return sum2 << 16 | sum1
+
+
+
+
+
+    return
 
 if __name__ == '__main__':
     main()
